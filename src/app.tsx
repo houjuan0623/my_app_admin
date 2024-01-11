097489d171ea0aa7afc1c18a6ab9e41095f46213
houjuan0623 <houjuan0623@gmail.com>
2024-01-11 09:34:34 +0000

    feat(request): 添加请求统一拦截

--- src/app.tsx
+++ src/app.tsx
@@ -5,8 +5,11 @@ import type { RunTimeLayoutConfig } from 'umi';
 import { history, Link } from 'umi';
 import RightContent from '@/components/RightContent';
 import Footer from '@/components/Footer';
-import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
+import { currentUser as queryCurrentUser } from './services/my-app';
 import { BookOutlined, LinkOutlined } from '@ant-design/icons';
+import { message } from 'antd';
+
+import type { RequestConfig } from 'umi';
 
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
@@ -103,4 +106,64 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
     },
     ...initialState?.settings,
   };
+};
+
+/** 请求拦截 */
+const requestInterceptor = (url: any, options: any): any => {
+  const headers = {
+    ...options.headers,
+    'Content-Type': 'application/json',
+    // TODO: 这里还需要重新修改登录的过程
+    // Authorization: `Bearer XXXXXXXXX`,
+  };
+  return {
+    url: url,
+    options: { ...options, headers: headers },
+  };
+};
+
+const codeMessage = {
+  500: '服务器发生错误，请检查服务器。',
+  502: '网关错误。',
+  503: '服务不可用，服务器暂时过载或维护。',
+  504: '网关超时。',
+};
+
+export const request: RequestConfig = {
+  timeout: 10000,
+  errorConfig: {
+    adaptor: (res: { statusCode: number; message: string; success: boolean; data?: object }) => {
+      if (typeof res === 'object') {
+        return {
+          ...res,
+          success: res.success,
+          errorMessage: res.message,
+        };
+      } else {
+        // 后端没有响应res
+        return {
+          success: false,
+          errorMessage: res,
+        };
+      }
+    },
+  },
+  middlewares: [],
+  credentials: 'include',
+  requestInterceptors: [requestInterceptor],
+  responseInterceptors: [
+    async (response) => {
+      if (!response) {
+        message.error('没有获取到响应，请重试');
+      }
+      if (response && response.status) {
+        if (codeMessage[response.status]) {
+          const errorText = codeMessage[response.status] || response.statusText;
+          const { status, url } = response;
+          message.error(`请求错误 ${status}: ${url}，详情：${errorText}`);
+        }
+      }
+      return response;
+    },
+  ],
 };
