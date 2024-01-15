f6139ce83d7b27ada1793c6ebd663000c2055fd0
houjuan0623 <houjuan0623@gmail.com>
2024-01-15 10:52:33 +0000

    fix(request): 添加CurrentUser路由返回值的定义

--- src/services/my-app/typings.d.ts
+++ src/services/my-app/typings.d.ts
@@ -22,4 +22,27 @@ declare namespace MYAPI {
     password?: string;
     autoLogin?: boolean;
   };
+  type CurrentUser = {
+    name?: string;
+    avatar?: string;
+    userid?: string;
+    email?: string;
+    signature?: string;
+    title?: string;
+    group?: string;
+    tags?: { key?: string; label?: string }[];
+    notifyCount?: number;
+    unreadCount?: number;
+    country?: string;
+    access?: string;
+    geographic?: {
+      province?: { label?: string; key?: string };
+      city?: { label?: string; key?: string };
+    };
+    address?: string;
+    phone?: string;
+    menuInfo?: Omit<MenuDataItem, 'routes'> & {
+      children?: Route[];
+    };
+  };
 }
