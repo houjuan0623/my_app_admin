cd47e23c56f7d3d34eee1ff81d3f5f3593de9cf3
houjuan0623 <houjuan0623@gmail.com>
2024-01-19 09:11:25 +0000

    fix(config): 修改部分配置和中文解释

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
   const intl = useIntl();
   const defaultMessage = intl.formatMessage({
     id: 'app.copyright.produced',
-    defaultMessage: '蚂蚁集团体验技术部出品',
+    defaultMessage: '某某公司',
   });
 
   const currentYear = new Date().getFullYear();
@@ -16,21 +16,15 @@ const Footer: React.FC = () => {
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: '1',
+          title: '链接1',
+          href: 'https://www.baidu.com',
           blankTarget: true,
         },
         {
           key: 'github',
           title: <GithubOutlined />,
           href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
           blankTarget: true,
         },
       ]}
